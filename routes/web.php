20ebad3405988fa21163cfd4be662e467883dbfc
hoangthach1402 <62414604+hoangthach1402@users.noreply.github.com>
2021-03-13 10:52:06 +0700

    add them route

--- routes/web.php
+++ routes/web.php
@@ -16,6 +16,7 @@ use Illuminate\Support\Facades\Route;
 | contains the "web" middleware group. Now create something great!
 |
 */
+Route::get('newbrand');
 Route::get('hellogit', function ($id) {
     
 });
